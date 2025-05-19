30c0e8c3275c8fa235545c9cf630631532047d39
Anh Quan <taquan076@gmail.com>
2025-05-19 14:00:19 +0700

    third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world! This is a simple React app.(hehe)
+          Hello world! .(hehe)
         </p>
         <a
           className="App-link"
